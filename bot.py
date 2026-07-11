5d9209428af8e6bc6e8b531a10f0938335d82733
rikodeko0275-bit <rikodeko0275@gmail.com>
2026-07-11 21:34:08 +0500

    Update bot token and remove error handler
    Replaced the environment variable for the bot token with a hardcoded value and removed the error handler.

--- bot.py
+++ bot.py
@@ -1,20 +1,14 @@
-import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = os.getenv("BOT_TOKEN")
-
-print("TOKEN:", TOKEN)
+TOKEN = "8807056466:AAH8g7hz1n3i9DTeePDAALiM-yRV6z7PCnc"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Сәлем! Бот жұмыс істеп тұр.")
 
-async def error_handler(update, context):
-    print("ERROR:", context.error)
-
 app = Application.builder().token(TOKEN).build()
+
 app.add_handler(CommandHandler("start", start))
-app.add_error_handler(error_handler)
 
 print("Bot started...")
 app.run_polling()
