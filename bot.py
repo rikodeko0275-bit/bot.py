c09a101a5d28134717658fa8cb4d8163e501683b
rikodeko0275-bit <rikodeko0275@gmail.com>
2026-07-11 21:29:00 +0500

    Implement error handler and logging
    Added error handling and logging for the bot.

--- bot.py
+++ bot.py
@@ -1,14 +1,20 @@
+import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
-import os
 
 TOKEN = os.getenv("BOT_TOKEN")
+
+print("TOKEN:", TOKEN)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Сәлем! Бот жұмыс істеп тұр.")
 
+async def error_handler(update, context):
+    print("ERROR:", context.error)
+
 app = Application.builder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
+app.add_error_handler(error_handler)
 
 print("Bot started...")
 app.run_polling()
