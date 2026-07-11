7089d7d658afed6792cf6e90d9a344f8337178a5
rikodeko0275-bit <rikodeko0275@gmail.com>
2026-07-11 21:09:48 +0500

    Update print statement for TOKEN in bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,4 @@
 import os
 
 TOKEN = os.getenv("BOT_TOKEN")
-
-print("TOKEN:", TOKEN)
-
-from telegram.ext import Application
-
-app = Application.builder().token(TOKEN).build()
-
-print("Bot started...")
-app.run_polling()
+print("TOKEN =", TOKEN)
